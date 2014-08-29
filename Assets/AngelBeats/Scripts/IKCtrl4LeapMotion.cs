8cb2555b1d505680542582133e636a2f18426e67
motoso <motosonomoto@gmail.com>
2014-08-30 02:07:44 +0900

    Bring Unity-chan's arm to my hand SLOWLY

--- Assets/AngelBeats/Scripts/IKCtrl4LeapMotion.cs
+++ Assets/AngelBeats/Scripts/IKCtrl4LeapMotion.cs
@@ -7,7 +7,9 @@ using System.Collections;
 public class IKCtrl4LeapMotion : MonoBehaviour {
 	protected Animator animator;
 	protected int isEmbarrassedId;
+	protected float IKGoalPosition = 0.0f;
 	public bool ikActive = false;
+
 
 	
 	void Start () 
@@ -15,13 +17,6 @@ public class IKCtrl4LeapMotion : MonoBehaviour {
 		animator = GetComponent<Animator>();
 		isEmbarrassedId = Animator.StringToHash("isEmbarrassed");
 	}
-
-	// void Update(){
-	// 	if(Input.GetKey("up")){
-	// 		OnAnimatorIK();
-	// 	}
-	// }
-        //IKを計算するコールバック
 
 	void Update()
 	{
@@ -35,11 +30,10 @@ public class IKCtrl4LeapMotion : MonoBehaviour {
 		}
 	}
 
-
 	void OnAnimatorIK()
 	{
 		if(animator) {
-                        //IKがアクティブならば、位置や回転を直接、目標物に設定します。
+            //IKがアクティブならば、位置や回転を直接、目標物に設定します。
 			if(ikActive) {
 				GameObject hand = GameObject.Find("SkeletalHand(Clone)/palm");
 				// GameObject hand = GameObject.Find("palm");
@@ -49,20 +43,27 @@ public class IKCtrl4LeapMotion : MonoBehaviour {
 					// Debug.Log(GameObject.Find("SkeletalHand(Clone)/palm"));
 					// Debug.Log("x" + hand.transform.position.x);
 
-                                //右手のweight = 1.0 は位置や回転が目標物に設定します（キャラクターが握りたい場所）
-					animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1.0f);
+                    //右手のweight = 1.0 は位置や回転が目標物に設定します（キャラクターが握りたい場所）
+					animator.SetIKPositionWeight(AvatarIKGoal.RightHand,IKGoalPosition);
 					animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1.0f);
-				// Debug.Log("ikActive");
+
+					// ゆっくり近づける
+					if(IKGoalPosition < 1.0f){
+						IKGoalPosition += 0.005f;
+					}
+
 			        //右手の位置および回転を外部オブジェクトの位置に設定する
 					if(rightHandObj != null) {
 						animator.SetIKPosition(AvatarIKGoal.RightHand,rightHandObj.position);
 						animator.SetIKRotation(AvatarIKGoal.RightHand,rightHandObj.rotation);
 					}					
+				}else{
+					//手がなくなったらゆっくり近づけるパラメタを初期化
+					IKGoalPosition = 0.0f;
 				}
 			}
-                        //IKがアクティブでないならば、位置や回転を元の場所に設定を戻します。
-			else {			
-				// Debug.Log("ikNotActive");
+			//IKがアクティブでないならば、位置や回転を元の場所に設定を戻します。
+			else {
 				animator.SetIKPositionWeight(AvatarIKGoal.RightHand,0);
 				animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0);				
 			}
